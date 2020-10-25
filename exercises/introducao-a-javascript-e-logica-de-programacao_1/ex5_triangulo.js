216b74f6bb8c7485ab2e5906f9d81895671678e7
Ronald Jr <rpintojunior@gmail.com>
2020-10-25 12:34:14 -0300

    Adiciona o code triangulo

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/ex5_triangulo.js
@@ -0,0 +1,11 @@
+let a = 30;
+let b = 90;
+let c = 60;
+
+let sum = a + b + c
+
+if (sum == 180) {
+    console.log("Essa soma corresponde a um triângulo.");
+} else {
+    console.log("Essa soma não corresponde a um triângulo");
+}
