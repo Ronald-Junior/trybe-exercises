a82adb02026a4f47101c9440f21f76d5744ddcbc
Ronald Jr <rpintojunior@gmail.com>
2020-10-25 12:57:56 -0300

    Adiciona o code se uma for impar

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/ex10_produto.js
@@ -0,0 +1,14 @@
+let custoProd = 25;
+let valVenda = 40;
+let custoCompras = custoProd*1000;
+let imposto = custoCompras*20/100;
+let custoTotal = custoCompras - imposto;
+let vendaTotal = valVenda*1000;
+let lucro = vendaTotal - custoTotal;
+
+if (custoProd < 0 || valVenda < 0) {
+  console.error("Números inválidos")
+}
+else {
+  console.log(lucro)
+}
