65dbdd3ae91b1ec495a11af509046fd119c6b271
Ronald Jr <rpintojunior@gmail.com>
2020-11-07 14:31:33 -0300

    Insere exercicio parte 1

--- exercises/javascript-dom-eventos-e-web-storage_3/exercises/5_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/exercises/5_3/script.js
@@ -14,3 +14,60 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+// Exercicio parte 1:
+const dezDaysList = [
+    29,
+    30,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+  ];
+  
+  const daysOfTheMonth = document.querySelector('#days');
+  
+  let monthDays = () => {
+    for (let day of dezDaysList) {
+      let listOfDays = document.createElement('li');
+      listOfDays.className = 'day';
+      listOfDays.innerHTML = day;
+      daysOfTheMonth.appendChild(listOfDays);
+  
+      if (day === 24 || day === 25 || day === 31) {
+        listOfDays.classList.add('holiday');
+      }
+  
+      if (day === 4 || day === 11 || day === 18 || day === 25) {
+        listOfDays.classList.add('friday');
+      }
+    }
+  };
+  monthDays();
+  
