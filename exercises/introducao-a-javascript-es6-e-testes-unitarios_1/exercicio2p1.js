02b5f0158a2aac4479354a9b4cb24c85d55f99c6
Ronald Jr <rpintojunior@gmail.com>
2020-11-27 17:02:38 -0300

    Insere exeercicio1 parte 2

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercicio2p1.js
@@ -0,0 +1,6 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const crescentSort = (array) => array.sort((a, b) => a - b);
+
+console.log(oddsAndEvens);
+console.log(`Os números ${crescentSort(oddsAndEvens)} se encontram ordenados de forma crescente!`);
